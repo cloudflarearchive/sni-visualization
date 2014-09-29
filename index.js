cda2fa54d940415f1095776295b31efdcd2cbc74
Terin Stock <terinjokes@gmail.com>
2014-09-29 11:33:07 -0700

    Switch to red-to-blue color scheme.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var Promise = require('es6-promise').Promise;
 var legendTemplate = require('./templates/legend.handlebars');
 var infoTemplate = require('./templates/info.handlebars');
 
-var colors = ['#9e2128', '#bd2527', '#ba4128', '#b8572b', '#b5692d', '#b27930', '#ae8932', '#aa9935', '#a6a938', '#a0b83b', '#9aca3e'];
+var colors = ["#a50026", "#d73027", "#f46d43", "#fdae61", "#fee090", "#e0f3f8", "#abd9e9", "#74add1", "#4575b4", "#313695"];
 var worldwideAvg = 0;
 var map, geojson, legend, info;
 
